5249e77eac3a819cf29d317decdbaa23c9a58d84
hanabee337 <hanabee337@gmail.com>
2017-03-28 17:11:04 +0900

    deezer login - url, view 구현
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login_fbv, name='login'),
+
+    url(r'^login/deezer/$', views.login_deezer, name='login_deezer'),
 ]
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,14 +1,20 @@
-from django.http import HttpResponse
-
-
 # Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from deezer.settings import config
 
 
 def login_fbv(request):
     # return HttpResponse('login view')
 
+    deezer_app_id = config['deezer']['secret_key']
     context = {
-
+        'deezer_app_id': deezer_app_id,
     }
     return render(request, 'member/login.html', context)
+
+
+def login_deezer(request):
+    print(request.GET)
+    return HttpResponse('login_deezer view')
