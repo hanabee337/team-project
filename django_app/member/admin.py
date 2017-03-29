67b1905b67b726e658eb3251604055c4e99bc025
hanabee337 <hanabee337@gmail.com>
2017-03-29 21:56:41 +0900

    Login - Model fields 추가 및 이에 따른 Migrations 그리고, 해당 필드 UserAdmin에도 반영
    migrations for new model and fields added such as username, password, email, gender, age
    
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
 from .models import MyUser
 
-admin.site.register(MyUser)
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email', 'gender', 'age', )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+    list_display = ('username', 'email',)
+
+
+admin.site.register(MyUser, MyUserAdmin)
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
+from django.db import models
+
+
 class MyUser(AbstractUser):
-    pass
+    CHOICES_GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    username = models.CharField(max_length=30, unique=True)
+    nickname = models.CharField(max_length=30)
+    email = models.EmailField(blank=False, null=False)
+    gender = models.CharField(max_length=1, choices=CHOICES_GENDER)
+    age = models.IntegerField(blank=True, null=True)
+
+    is_staff = models.BooleanField(default=False)
