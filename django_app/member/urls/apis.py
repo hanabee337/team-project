b425c33b606b47cf30fb601f8425746db72d0878
hanabee337 <hanabee337@gmail.com>
2017-04-01 22:27:43 +0900

    DeleteAuthToken 추가
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework.authtoken import views as auth_token_view
 
+from member.apis.signup import SignUp_cbv
+from member.apis.authtoken import DeleteAuthToken
 from member.apis.login import LogoutView
-from .. import apis as views
 
 urlpatterns = [
-    url(r'^signup/$', views.SignUp_cbv.as_view(), name='signup'),
+    url(r'^signup/$', SignUp_cbv.as_view(), name='signup'),
     url(r'^token-auth/$', auth_token_view.obtain_auth_token),
+    url(r'^token-delete/$', DeleteAuthToken.as_view()),
 
     # login url would be like this, api/memmber/login/
     # logout url would be like that, api/member/logout/
