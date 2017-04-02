636fef55563f9b9625d19bbe9f8192bcf4b9243a
hanabee337 <hanabee337@gmail.com>
2017-04-03 04:00:07 +0900

    DEBUG mode를 deploy용과 구분
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/deezer/settings.py
+++ django_app/deezer/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import json
 import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.environ.get('MODE') == 'DEBUG'
+print('DEBUG:{}'.format(DEBUG))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
