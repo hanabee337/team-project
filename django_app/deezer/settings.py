f64cc183c63a52c70742a706c186ca49fd6e3b12
hanabee337 <hanabee337@gmail.com>
2017-04-03 03:23:43 +0900

    pgadmin용 db설정 및 migration, migrations 에러로 인한 pyscopg2 패키지 설치
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/deezer/settings.py
+++ django_app/deezer/settings.py
@@ -130,9 +130,18 @@ WSGI_APPLICATION = 'deezer.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config['db']['engine'],
+        'NAME': config['db']['name'],
+        'USER': config['db']['user'],
+        'PASSWORD': config['db']['password'],
+        'HOST': config['db']['host'],
+        'PORT': config['db']['port']
     }
 }
 
