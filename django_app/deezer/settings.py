4f2515b70ecbdd359ad2bfedf92c0f554f18438b
hanabee337 <hanabee337@gmail.com>
2017-04-03 17:08:10 +0900

    DB를 sqlite3로 원복
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/deezer/settings.py
+++ django_app/deezer/settings.py
@@ -131,19 +131,19 @@ WSGI_APPLICATION = 'deezer.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
     'default': {
-        'ENGINE': config['db']['engine'],
-        'NAME': config['db']['name'],
-        'USER': config['db']['user'],
-        'PASSWORD': config['db']['password'],
-        'HOST': config['db']['host'],
-        'PORT': config['db']['port']
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # 'default': {
+    #     'ENGINE': config['db']['engine'],
+    #     'NAME': config['db']['name'],
+    #     'USER': config['db']['user'],
+    #     'PASSWORD': config['db']['password'],
+    #     'HOST': config['db']['host'],
+    #     'PORT': config['db']['port']
+    # }
 }
 
 
