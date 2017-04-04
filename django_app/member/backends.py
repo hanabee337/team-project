39c173debee31c5cbadc375e9a60d6b0cfebe4b5
hanabee337 <hanabee337@gmail.com>
2017-04-04 22:10:05 +0900

    when logout, there is no email argument, so, 'user_can_authenticate' is needed to prevent server error 500
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/backends.py
+++ django_app/member/backends.py
@@ -82,3 +82,11 @@ class UserModelBackend(object):
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
+
+    def user_can_authenticate(self, user):
+        """
+        Reject users with is_active=False. Custom user models that don't have
+        that attribute are allowed.
+        """
+        is_active = getattr(user, 'is_active', None)
+        return is_active or is_active is None
