414da8b2be4384d965f20889233613e5668f9be7
hanabee337 <hanabee337@gmail.com>
2017-04-07 21:20:48 +0900

    CORS 설정 추가
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/deezer/settings.py
+++ django_app/deezer/settings.py
@@ -121,6 +121,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_auth',
 
+    'corsheaders',
+
     'storages',
 
     'member',
@@ -130,6 +132,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,6 +141,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+)
 
 ROOT_URLCONF = 'deezer.urls'
 
