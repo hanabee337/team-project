081da4fead4b21aaa0e341e10cd5befc13f60854
cdragon-1 <streetfood89@gmail.com>
2017-04-08 17:57:33 +0900

    api_view의 post요청, form으로 올 시에 db에 데이터 적용되는 기능 구현 완료

--- django_app/search/views.py
+++ django_app/search/views.py
@@ -10,14 +10,13 @@ from search.models import Music
 from search.serializers import MusicSerializer
 
 
-
 @api_view(['GET', 'POST'])
 def search(request, format=None):
     musics = []
     context = {
         'musics': musics,
     }
-    r = requests.get('https://api.deezer.com/search?q=madonna')
+    r = requests.get('https://api.deezer.com/search?q=cindy')
     result = r.text
     result_dict = json.loads(result)
 
@@ -43,72 +42,21 @@ def search(request, format=None):
             'album_picture': album_picture,
         }
         data = musics.append(cur_item_dict)
-        # pprint(musics)
+        # print('data: {}'.format(data))
         # jdata = musics.append(cur_item_dict)
         # bio_data = BytesIO(jdata)
         # data = JSONParser().parse(bio_data)
 
-
     if request.method == 'GET':
-        snippets = Music.objects.all()
+        musics = Music.objects.all()
         serializer = MusicSerializer(musics, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = MusicSerializer(data=request.data, many=True)
+        # serializer = MusicSerializer(data=request.data, many=True)
+        serializer = MusicSerializer(data=musics, many=True)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# def search_from_deezer(keyword, page_token=None):
-#     deezer_api_key = config['deezer']['API_KEY_DEEZER']
-#     params = {
-#         'q': keyword,
-#         'maxResults': 30,
-#         'type': 'artist',
-#         'key': deezer_api_key,
-#     }
-#
-#     r = requests.get('https://apis.deezer.com/2.0/search?q=', params=params)
-#     result = r.text
-#
-#     # 해당 내용을 다시 json.loads()를 이용해 파이썬 객체로 변환
-#     result_dict = json.loads(result)
-#     return result_dict
-#
-#
-# def search(request):
-#     musics = []
-#     context = {
-#         'musics': musics,
-#     }
-#
-#     keyword = request.GET.get('keyword', '').strip()
-#
-#     if keyword != '':
-#         search_result = search_from_deezer(keyword)
-#
-#         context['keyword'] = keyword
-#
-#         items = search_result['data']
-#         pprint(items)
-#         for item in items:
-#             # 실제로 사용할 데이터
-#             artist = item['artist']['name']
-#             title = item['title']
-#             preview = item['preview']
-#             artist_picture = item['artist']['picture_small']
-#             album_picture = item['album']['cover_small']
-#
-#             cur_item_dict = {
-#                 'artist': artist,
-#                 'title': title,
-#                 'preview': preview,
-#                 'artist_picture': artist_picture,
-#                 'album_picture': album_picture,
-#             }
-#             musics.append(cur_item_dict)
-#             pprint(musics)
-#
-#     return render(request, 'music/search.html', context)
