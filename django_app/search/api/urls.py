8616285eff376ca04e2bf00571a27b9d74afccaa
cdragon-1 <streetfood89@gmail.com>
2017-04-11 20:11:09 +0900

    createapiview 기능 복원을 위한 코드 재생성

--- django_app/search/api/urls.py
+++ django_app/search/api/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 
 from .views import (
     MusicListAPIView,
-)
+    MusicCreateAPIView)
 
 urlpatterns = [
     url(r'^list/', MusicListAPIView.as_view(), name='music_search_list'),
+    url(r'^create/', MusicCreateAPIView.as_view(), name='music_create'),
 ]
--- django_app/search/api/views.py
+++ django_app/search/api/views.py
@@ -1,15 +1,21 @@
 from django.db.models import Q
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 
 from search.models import Music
-from .serializers import MusicListSerializer
+from .serializers import MusicListSerializer, MusicCreateSerializer
 
 
 class MusicListAPIView(ListAPIView):
     serializer_class = MusicListSerializer
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'artist']
+
+    def list(self, request, *args, **kwargs):
+        # 우리db에서 리스트 결과를 돌려주기전에 해야할 일을 추가
+        # request.query_params
+        # request.data
+        return super().list(request, *args, **kwargs)
 
     def get_queryset(self, *args, **kwargs):
         queryset_list = Music.objects.all()
@@ -20,3 +26,11 @@ class MusicListAPIView(ListAPIView):
                 Q(artist__icontains=query)
             ).distinct()
         return queryset_list
+
+
+class MusicCreateAPIView(CreateAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
