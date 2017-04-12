ec31a28be196911337c0563ed1f175436d6c8cda
atlantissss <ilikereport@gmail.com>
2017-04-12 23:13:44 +0900

    search 수정내역 및 .gitignore, settings_local 반영

--- django_app/search/api/urls.py
+++ django_app/search/api/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 
 from .views import (
     MusicListAPIView,
+    MusicCreateAPIView
 )
 
 urlpatterns = [
     url(r'^list/', MusicListAPIView.as_view(), name='music_search_list'),
+    # 임시
+    url(r'^create/', MusicCreateAPIView.as_view(), name='music_search_create'),
 ]
--- django_app/search/api/views.py
+++ django_app/search/api/views.py
@@ -1,9 +1,10 @@
 from django.db.models import Q
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from search.models import Music
-from .serializers import MusicListSerializer
+from .serializers import MusicListSerializer, MusicCreateSerializer
 
 
 class MusicListAPIView(ListAPIView):
@@ -20,3 +21,13 @@ class MusicListAPIView(ListAPIView):
                 Q(artist__icontains=query)
             ).distinct()
         return queryset_list
+
+
+# 임시
+class MusicCreateAPIView(CreateAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
