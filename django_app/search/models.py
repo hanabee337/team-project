8eb0e929b9ed5070db5c1399d541260651e8fb80
cdragon-1 <streetfood89@gmail.com>
2017-04-14 05:22:04 +0900

    album_id와 album_title 필드를 추가함

--- django_app/search/models.py
+++ django_app/search/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Music(models.Model):
-    id_num = models.CharField(max_length=255)
+    song_id = models.CharField(max_length=255)
     rank = models.CharField(max_length=255)
     duration = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
@@ -13,6 +13,8 @@ class Music(models.Model):
     artist_picture_medium = models.TextField(max_length=500)
     artist_picture_big = models.TextField(max_length=500)
 
+    album_id = models.CharField(max_length=255)
+    album_title = models.CharField(max_length=255)
     album_picture_small = models.TextField(max_length=500)
     album_picture_medium = models.TextField(max_length=500)
     album_picture_big = models.TextField(max_length=500)
