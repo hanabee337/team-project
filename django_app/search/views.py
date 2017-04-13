64b384cd13f2bef5a56b884373f3f0a791c8fac9
cdragon-1 <streetfood89@gmail.com>
2017-04-14 05:24:34 +0900

    singers를 돌면서 Deezer의 전체 결과를 for loop하는 로직 완성

--- django_app/search/views.py
+++ django_app/search/views.py
@@ -12,65 +12,68 @@ from search.serializers import MusicSerializer
 @api_view(['GET', 'POST'])
 def search(request, format=None):
     musics = []
-
-    singers = {
-        'drake', 'rick ross', 'big sean',
-        'ed sheeran', 'kodak black', 'brono mars', 'sound track',
-        'mastodon', 'future', 'migos', 'mercyme', 'the weeknd',
-        'miranda lambert', 'keith urban', 'post malone', 'khalid'
-    }
+    singers = {'louis',
+               }
 
     for singer in singers:
-        r = requests.get('https://api.deezer.com/search?q={}'.format(singer))
-        result = r.text
-        # pprint('result: {}'.format(result))
+        initial_r = requests.get('https://api.deezer.com/search?q={}'.format(singer))
+        result = initial_r.text
         result_dict = json.loads(result)
-
         search_result = result_dict
-        # pprint('search_result: {}'.format(search_result))
+        total_results = search_result['total']
+        print(total_results)
 
-        items = search_result['data']
-        # pprint('items: {}'.format(items))
+        page_num = 0
+        while page_num < total_results:
+            starting_r = requests.get('https://api.deezer.com/search?q={}&index={}'.format(singer, page_num))
 
-        for item in items:
-            # 실제로 사용할 데이터
-            id_num = item['id']
-            rank = item['rank']
-            duration = item['duration']
-            title = item['title_short']
-            artist = item['artist']['name']
-            preview = item['preview']
-            artist_picture_small = item['artist']['picture_small']
-            artist_picture_medium = item['artist']['picture_medium']
-            artist_picture_big = item['artist']['picture_big']
+            result = starting_r.text
+            result_dict = json.loads(result)
 
-            album_picture_small = item['album']['cover_small']
-            album_picture_medium = item['album']['cover_medium']
-            album_picture_big = item['album']['cover_big']
+            search_result = result_dict
+            total_results = search_result['total']
 
-            cur_item_dict = {
-                'id_num': id_num,
-                'rank': rank,
-                'duration': duration,
-                'artist': artist,
-                'title': title,
-                'preview': preview,
+            items = search_result['data']
 
-                'artist_picture_small': artist_picture_small,
-                'artist_picture_medium': artist_picture_medium,
-                'artist_picture_big': artist_picture_big,
+            for item in items:
+                # 실제로 사용할 데이터
+                song_id = item['id']
+                rank = item['rank']
+                duration = item['duration']
+                title = item['title_short']
+                artist = item['artist']['name']
+                preview = item['preview']
+                artist_picture_small = item['artist']['picture_small']
+                artist_picture_medium = item['artist']['picture_medium']
+                artist_picture_big = item['artist']['picture_big']
 
-                'album_picture_small': album_picture_small,
-                'album_picture_medium': album_picture_medium,
-                'album_picture_big': album_picture_big,
+                album_id = item['album']['id']
+                album_title = item['album']['title']
+                album_picture_small = item['album']['cover_small']
+                album_picture_medium = item['album']['cover_medium']
+                album_picture_big = item['album']['cover_big']
 
-            }
+                cur_item_dict = {
+                    'song_id': song_id,
+                    'rank': rank,
+                    'duration': duration,
+                    'artist': artist,
+                    'title': title,
+                    'preview': preview,
 
-            data = musics.append(cur_item_dict)
-            # pprint('musics: {}'.format(musics))
-    # jdata = musics.append(cur_item_dict)
-    # bio_data = BytesIO(jdata)
-    # data = JSONParser().parse(bio_data)
+                    'artist_picture_small': artist_picture_small,
+                    'artist_picture_medium': artist_picture_medium,
+                    'artist_picture_big': artist_picture_big,
+
+                    'album_id': album_id,
+                    'album_title': album_title,
+                    'album_picture_small': album_picture_small,
+                    'album_picture_medium': album_picture_medium,
+                    'album_picture_big': album_picture_big,
+                }
+                data = musics.append(cur_item_dict)
+
+            page_num += 25
 
     if request.method == 'GET':
         musics = Music.objects.all()
@@ -78,7 +81,6 @@ def search(request, format=None):
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        # serializer = MusicSerializer(data=request.data, many=True)
         serializer = MusicSerializer(data=musics, many=True)
 
         if serializer.is_valid():
