529e46d623f2f4b48de0605b645b838e1d84d677
atlantissss <ilikereport@gmail.com>
2017-04-13 19:51:00 +0900

    playlist - playlist app 내부에서 url 병합

--- django_app/playlist/urls/apis.py
+++ django_app/playlist/urls/apis.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
+from playlist import views
 from .. import apis
 
 urlpatterns = [
     url(r'^$', apis.PlayListListView.as_view(), name='playlist-list'),
-    url(r'^(?P<pk>[0-9]+)/$', apis.PlayListDetailView.as_view(), name='playlist-detail'),
+    url(r'^(?P<pk>[0-9]+)$', apis.PlayListDetailView.as_view(), name='playlist-detail'),
+    url(r'^select_my_playlist', apis.select_my_playlist),
+    url(r'^add_to_my_playlist', apis.add_to_my_playlist),
+    url(r'^views_add_to_my_playlist', views.views_add_to_my_playlist),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
