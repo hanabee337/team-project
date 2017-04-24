d2719647f87a73560ba3fd2c1071405ad1f87f1b
hanabee337 <hanabee337@gmail.com>
2017-04-24 16:54:57 +0900

    [Facebook] Facebook 연동 회원가입 using Access Token
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/apis/signup.py
+++ django_app/member/apis/signup.py
@@ -1,8 +1,12 @@
+from pprint import pprint
+
+import requests
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 
+from deezer import settings
 from member.serializers import UserSerializer, SignupSerializer, Facebook_SignUp_Serializer
 
 User_model = get_user_model()
@@ -72,29 +76,93 @@ class Facebook_SignUp_View(generics.CreateAPIView):
         return self.create(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
-        # print('request.data:{}'.format(request.data))
+        # print('CREATE request.data:{}'.format(request.data))
 
-        # First, validate request.data in SignupSerializer
-        signup_serializer = self.get_serializer(data=request.data)
-        signup_serializer.initial_data['password'] = request.data.get('email')
-        signup_serializer.is_valid(raise_exception=True)
-        # print('\nsignup_serializer.validated_data:{}\n'.format(signup_serializer.validated_data))
+        APP_ID = settings.config['facebook']['app_id']
+        SECRET_CODE = settings.config['facebook']['secret_code']
+        # REDIRECT_URI = 'http://localhost:8000/member/login/facebook/'
+        APP_ACCESS_TOKEN = '{app_id}|{secret_code}'.format(
+            app_id=APP_ID,
+            secret_code=SECRET_CODE,
+        )
+        USER_ACCESS_TOKEN = request.data.get('email')
 
-        self.perform_create(signup_serializer)
-
-        user = User_model.objects.get(email__iexact=request.data.get('email'))
-        user_info = {
-            'email': user.email,
-            'nickname': user.nickname,
-            'user_type': user.user_type,
-            'age': user.age,
-            'gender': user.gender,
+        # Step 3: debug_token을 얻어오기
+        # 유저 액세스 토큰과 앱 엑세스 토큰을 사용해서 토큰 검증을 거친다
+        url_debug_token = 'https://graph.facebook.com/debug_token?'
+        params = {
+            'input_token': USER_ACCESS_TOKEN,
+            'access_token': APP_ACCESS_TOKEN,
         }
-        # print('user_info:{}'.format(user_info))
 
-        # print('Facebook_SignUp_View:{}'.format(data))
-        return Response({'user_info': user_info}, status=status.HTTP_201_CREATED)
+        r = requests.get(url_debug_token, params=params)
+        dict_debug_token = r.json()
+        pprint(dict_debug_token)
+
+        if dict_debug_token['data']['is_valid']:
+            USER_ID = dict_debug_token['data']['user_id']
+            print('USER_ID : %s' % USER_ID)
+
+            # 해당 USER_ID로 graph API에 유저정보를 요청
+            url_api_user = 'https://graph.facebook.com/{user_id}'.format(
+                user_id=USER_ID
+            )
+            fields = [
+                'id',
+                'first_name',
+                'last_name',
+                'gender',
+                'picture',
+                'email',
+            ]
+            params = {
+                'fields': ','.join(fields),
+                'access_token': USER_ACCESS_TOKEN,
+            }
+            r = requests.get(url_api_user, params)
+            dict_user_info = r.json()
+            pprint(dict_user_info)
+
+            # First, validate request.data in SignupSerializer
+            signup_serializer = self.get_serializer(data=request.data)
+
+            # user id가 unique 하므로, email 과 password 로 활용
+            # signup_serializer.initial_data['email'] = USER_ID
+            # signup_serializer.initial_data['password'] = USER_ID
+            signup_serializer.initial_data = {
+                'email': USER_ID,
+                'password': USER_ID,
+                'nickname': request.data.get('nickname'),
+                # 'gender': request.data.get('gender', ''),
+                # 'age': request.data.get('age', '')
+            }
+
+            signup_serializer.is_valid(raise_exception=True)
+            # print('\n signup_serializer.validated_data:{}\n'.format(signup_serializer.validated_data))
+
+            # access token 을 email 란으로 입력 받은 후, 다시 user id로 바꿔치기.
+
+            if User_model.objects.filter(email__iexact=USER_ID).exists():
+                # 로그인 루틴을 여기서 처리하는 것으로..
+                # 회원 가입과 로그인(Token key 생성, password check)을 하나의 url에서 처리하게...
+                return Response({'이미 가입된 사용자 입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            self.perform_create(signup_serializer)
+
+            user = User_model.objects.get(email__iexact=USER_ID)
+            user_info = {
+                'email': user.email,
+                'nickname': user.nickname,
+                'user_type': user.user_type,
+                'gender': user.gender,
+                'age': user.age,
+            }
+            print('user_info:{}'.format(user_info))
+            # print('Facebook_SignUp_View:{}'.format(data))
+            return Response({'user_info': user_info}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'Facebook에서 유효한 User ID를 얻는데 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
-        # print('\nperform_create\n')
+        # print('\n perform_create\n')
         serializer.save()
